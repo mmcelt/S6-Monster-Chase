f470b4bd9ed8a279b72d7042dc85a9265c0d5526
mmcelt <mmcelt@yahoo.com>
2021-03-28 12:16:32 -0600

    28 - Camera Follow Player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	#region Fields & Properties
+
+	[SerializeField] float _minX, _maxX;
+
+	Transform _player;
+	Vector3 _tempPos;
+
+	#endregion
+
+	#region Getters
+
+
+	#endregion
+
+	#region Unity Methods
+
+	void Start() 
+	{
+		_player = GameObject.FindWithTag("Player").transform;
+	}
+	
+	void LateUpdate() 
+	{
+		_tempPos = transform.position;
+		_tempPos.x = _player.position.x;
+
+		_tempPos.x = Mathf.Clamp(_tempPos.x, -61.5f, 61.5f);
+
+		transform.position =_tempPos;
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
