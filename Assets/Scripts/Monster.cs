5ef693b208bbd78246037bd438894ef417bfa2ed
mmcelt <mmcelt@yahoo.com>
2021-03-28 14:12:34 -0600

    30 - Enemy Script

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+	#region Fields & Properties
+
+	[HideInInspector] public float _speed;
+
+	Rigidbody2D _theRB;
+
+	#endregion
+
+	#region Getters
+
+
+	#endregion
+
+	#region Unity Methods
+
+	void Awake()
+	{
+		_theRB = GetComponent<Rigidbody2D>();
+		_speed = 7f;
+	}
+
+	void Start() 
+	{
+		
+	}
+	
+	void FixedUpdate() 
+	{
+		_theRB.velocity = new Vector2(_speed, _theRB.velocity.y);
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
